1e9f56e958cf1e4192beaa6ddf8de0a347b8ca36
Brendan <brendanlawless@hotmail.com>
2024-10-21 11:16:45 +0100

    classes completed

--- src/battleships.ts
+++ src/battleships.ts
@@ -1,37 +1,28 @@
 import Player from "./player";
 
 export default class Battleship {
-    player1: Player;
-    player2: Player;
+  player1: Player;
+  player2: Player;
 
-    constructor() {
-        this.player1 = new Player();
-        this.player2 = new Player();
+  constructor() {
+    this.player1 = new Player();
+    this.player2 = new Player();
+  }
 
-        this.player1.playerBoard.defaultShipPlacement()
-        this.player2.playerBoard.defaultShipPlacement()
+  printBoard(player: Player) {
+    const self = player;
+
+    for (let row = 0; row <= 9; row++) {
+      let r = "";
+      for (let col = 0; col <= 9; col++) {
+        const shipKey = self.playerBoard.board[row][col];
+        let shipSunk: boolean
+
+        if (!isNaN(shipKey)) shipSunk = self.playerBoard.ships[shipKey].isSunk;
+
+        shipSunk || shipKey === undefined ? (r += "-") : (r += "s");
+      }
+      console.log(r);
     }
-
-    printBoard(player: Player) {
-
-        const self = player
-
-        for (let row = 0; row <= 9; row++) {
-            let r = "";
-            for (let col = 0; col <= 9; col++) {
-
-                const shipKey = self.playerBoard.board[row][col];
-                let shipSunk;
-
-                if (!isNaN(shipKey)) shipSunk = self.playerBoard.ships[shipKey].isSunk;
-                
-                if (shipSunk || shipKey === undefined) {
-                    r += "-";
-                } else {
-                    r += "s";
-                }
-            }
-            console.log(r);
-        }
-    }
+  }
 }
