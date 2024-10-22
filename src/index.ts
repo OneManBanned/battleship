c8a7736d1b11b2eac58a7b2eff50e312d4c40a07
Brendan <brendanlawless@hotmail.com>
2024-10-22 11:05:02 +0100

    PrintScreen moved out of Battleship class

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,53 @@
 import pkg from "prompt-sync";
 const prompt = pkg();
 
-import Battleships from "./battleships"
+import Battleships from "./battleships";
+import Player from "./player";
 
-const battleship = new Battleships()
-const player1 = battleship.player1
-const player2 = battleship.player2
+const battleship = new Battleships();
+const player1 = battleship.player1;
+const player2 = battleship.player2;
 
-player1.playerBoard.defaultShipPlacement()
-player2.playerBoard.defaultShipPlacement()
+player1.playerBoard.defaultShipPlacement();
+player2.playerBoard.defaultShipPlacement();
+
+function printBoard(player: Player) {
+  const self = player;
+
+  for (let row = 0; row <= 9; row++) {
+    let r = "";
+    for (let col = 0; col <= 9; col++) {
+      const shipKey = self.playerBoard.board[row][col];
+      let shipSunk: boolean;
+
+      if (!isNaN(shipKey)) shipSunk = self.playerBoard.ships[shipKey].isSunk;
+
+      shipSunk || shipKey === undefined ? (r += "-") : (r += "s");
+    }
+    console.log(r);
+  }
+}
 
 function inputCoordinate(axis: string): string {
-    return prompt(`Enter ${axis} number you wish to fire on: `)
+  return prompt(`Enter ${axis} number you wish to fire on: `);
 }
 
+while (!player1.playerBoard.shipsSunk() && !player2.playerBoard.shipsSunk()) {
+  const coordinates = { x: 0, y: 0 };
 
-while(!player1.playerBoard.shipsSunk() && !player2.playerBoard.shipsSunk()) {
+  printBoard(player1);
 
-    const coordinates = {x: 0, y: 0}
+  console.log("\n");
 
-    battleship.printBoard(player1)
+  printBoard(player2);
 
-    console.log("\n")
+  coordinates.x = Number(inputCoordinate("x").trim());
+  coordinates.y = Number(inputCoordinate("y").trim());
 
-    battleship.printBoard(player2)
-
-    coordinates.x = Number(inputCoordinate('x').trim())
-    coordinates.y = Number(inputCoordinate('y').trim())
-
-    player1.fire(player2, coordinates)
+  player1.fire(player2, coordinates);
 }
 
-console.log("\n", "GAME OVER")
-player1.playerBoard.shipsSunk() ? console.log("Player 2 Won") : console.log("Player 1 Won")
+console.log("\n", "GAME OVER");
+player1.playerBoard.shipsSunk()
+  ? console.log("Player 2 Won")
+  : console.log("Player 1 Won");
