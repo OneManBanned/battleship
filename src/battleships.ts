3fcddbcace418e80522ede8f562bf6f751037e29
Brendan <brendanlawless@hotmail.com>
2024-10-23 11:46:29 +0100

    Removed printBoard logic from class

--- src/battleships.ts
+++ src/battleships.ts
@@ -9,20 +9,4 @@ export default class Battleship {
     this.player2 = new Player();
   }
 
-  printBoard(player: Player) {
-    const self = player;
-
-    for (let row = 0; row <= 9; row++) {
-      let r = "";
-      for (let col = 0; col <= 9; col++) {
-        const shipKey = self.playerBoard.board[row][col];
-        let shipSunk: boolean
-
-        if (!isNaN(shipKey)) shipSunk = self.playerBoard.ships[shipKey].isSunk;
-
-        shipSunk || shipKey === undefined ? (r += "-") : (r += "s");
-      }
-      console.log(r);
-    }
-  }
 }
