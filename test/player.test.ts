4d1b7ba1a68bb1ffc28f0074c2a5e7b26fa58617
Brendan <brendanlawless@hotmail.com>
2024-10-27 12:06:18 +0000

    test: player.fire should set shotsFired grid coordinates to true

--- test/player.test.ts
+++ test/player.test.ts
@@ -9,8 +9,8 @@ describe("Player class", () => {
   });
 
   test("Player class should create instance of playerBoard", () => {
-      expect(p.playerBoard).toEqual(new Gameboard);
-  })
+    expect(p.playerBoard).toEqual(new Gameboard());
+  });
 
   test("fire should call opponent.playerBoard.checkGrid", () => {
     const opponent = new Player();
@@ -33,32 +33,36 @@ describe("Player class", () => {
   });
 
   test("fire should decrease length of ship, if coordinates hold a ship", () => {
-      const opponent = new Player()
-      const points = {x: 0, y: 0}
-      opponent.playerBoard.placeShip({x: 0, y: 0}, 0)
+    const opponent = new Player();
+    const points = { x: 0, y: 0 };
+    opponent.playerBoard.placeShip({ x: 0, y: 0 }, 0);
 
-      p.fire(opponent, points)
+    p.fire(opponent, points);
 
-     expect(opponent.playerBoard.ships[0].length).toBe(0) 
+    expect(opponent.playerBoard.ships[0].length).toBe(0);
   });
 
   test("fire should return undefined if grid coordinates are empty", () => {
-      const opponent = new Player()
-      const points = {x: 0, y: 0}
+    const opponent = new Player();
+    const points = { x: 0, y: 0 };
 
-      expect(p.fire(opponent, points)).toBe(undefined)
+    expect(p.fire(opponent, points)).toBe(undefined);
   });
 
   test("fire should should sink ship if ship length is 0 after being hit", () => {
-      const opponent = new Player()
-      const points = {x: 0, y: 0}
-      opponent.playerBoard.placeShip({x: 0, y: 0}, 0)
+    const opponent = new Player();
+    const points = { x: 0, y: 0 };
+    opponent.playerBoard.placeShip({ x: 0, y: 0 }, 0);
 
-      p.fire(opponent, points)
+    p.fire(opponent, points);
 
-     expect(opponent.playerBoard.ships[0].isSunk).toBe(true) 
+    expect(opponent.playerBoard.ships[0].isSunk).toBe(true);
+  });
 
-  })
+  test("fire should set given coordinates of shotsFired to true", () => {
+    const playerDummy = new Player()
+    p.fire(playerDummy, { x: 0, y: 0 });
 
-
+    expect(p.shotsFired[0][0]).toBe(true);
+  });
 });
