fa2483ecf04f55a64f67dbb6ade930777ad6e7dd
Brendan <brendanlawless@hotmail.com>
2024-10-27 11:04:27 +0000

    refactor: change turn variable to less ambiguous name
    - change turn variable to alternateTurn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Player from "./classes/player";
 import inputCoordinate from "./input/getInput";
 import ConsoleUI from "./ui/consoleInterface";
-import UI from "./ui/consoleInterface";
 
 const player1 = new Player() 
 const player2 = new Player()
@@ -10,12 +9,12 @@ const ui =  ConsoleUI(player1, player2)
 player1.playerBoard.defaultShipPlacement();
 player2.playerBoard.defaultShipPlacement();
 
-let turn = true
+let alternateTurn = true
 
 while (!player1.playerBoard.shipsSunk() && !player2.playerBoard.shipsSunk()) {
 
-  const currentPlayer = turn ? player1 : player2
-  const currentTarget = !turn ? player1 : player2
+  const currentPlayer = alternateTurn ? player1 : player2
+  const currentTarget = !alternateTurn ? player1 : player2
 
   const coordinates = { x: 0, y: 0 };
 
@@ -26,7 +25,7 @@ while (!player1.playerBoard.shipsSunk() && !player2.playerBoard.shipsSunk()) {
 
   currentPlayer.fire(currentTarget, coordinates);
 
-  turn = !turn
+  alternateTurn = !alternateTurn
 }
 
 ui.printWinner()
