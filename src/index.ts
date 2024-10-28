2eca6e05d339f4498c9c6fece9befa6d92ef1931
Brendan <brendanlawless@hotmail.com>
2024-10-28 10:18:42 +0000

    fix: player able to input same coordinates twice
    - no longer able to input same coordinates twice
    - message prints to console informing player that they have already
      fired at given coordinates.

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,35 @@ import Player from "./classes/player";
 import inputCoordinate from "./input/getInput";
 import ConsoleUI from "./ui/consoleInterface";
 
-const player1 = new Player() 
-const player2 = new Player()
-const ui =  ConsoleUI(player1, player2)
+const player1 = new Player();
+const player2 = new Player();
+const ui = ConsoleUI(player1, player2);
 
 player1.playerBoard.defaultShipPlacement();
 player2.playerBoard.defaultShipPlacement();
 
-let alternateTurn = true
+let alternateTurn = false;
 
 while (!player1.playerBoard.shipsSunk() && !player2.playerBoard.shipsSunk()) {
 
-  const currentPlayer = alternateTurn ? player1 : player2
-  const currentTarget = !alternateTurn ? player1 : player2
-
+  const currentPlayer = alternateTurn ? player1 : player2;
+  const currentTarget = !alternateTurn ? player1 : player2;
   const coordinates = { x: 0, y: 0 };
 
-  ui.printBoards()
+  ui.printBoards();
 
+  do {
   coordinates.x = Number(inputCoordinate("x"));
   coordinates.y = Number(inputCoordinate("y"));
+  
+  if (currentPlayer.shotsFired[coordinates.x][coordinates.y]) 
+      console.log("you have already fired at these coordinates")
+
+  } while (currentPlayer.shotsFired[coordinates.x][coordinates.y])
 
   currentPlayer.fire(currentTarget, coordinates);
 
-  alternateTurn = !alternateTurn
+  alternateTurn = !alternateTurn;
 }
 
-ui.printWinner()
+ui.printWinner();
